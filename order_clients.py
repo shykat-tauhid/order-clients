607a426754559be41b889da5d63f28507b35bbab
shykat <shykatibna@gmail.com>
2026-03-19 11:50:35 +0100

    added new client and method for the richest

--- order_clients.py
+++ order_clients.py
@@ -8,12 +8,30 @@ def read_json(f:str)->list:
 def order_by_priority(json_list:list)->list:
     return sorted(json_list, key=lambda x: x['priorita'], reverse=True)
 
+def add_new_client(new:dict, data:list[dict])->list:
+    data.append(new)
+    return order_by_priority(data)
+
+def get_highest_heritage(l:list[dict])->dict:
+    return sorted(l, key=lambda x: x['patrimonio'])[-1]
+
+
 def main():
     data = read_json('clienti.json')
-    ordered = order_by_priority(data)
+    new_client = {
+        "nome": "Luca",
+        "cognome": "Rossi",
+        "eta": 100,
+        "patrimonio": 840133.69,
+        "priorita": 6
+    }
+    ordered = add_new_client(new_client, data)
 
     for row in ordered:
         print(row)
+    
+    print('\n')
+    print(f"Richest: ", get_highest_heritage(data))
 
 
 if __name__ == "__main__":
