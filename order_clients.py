63f65fabf384800c2f8b907d1275916dde12dd5a
shykat <shykatibna@gmail.com>
2026-03-19 15:17:41 +0100

    added sell() method

--- order_clients.py
+++ order_clients.py
@@ -15,16 +15,31 @@ def add_new_client(new:dict, data:list[dict])->list:
 def get_highest_heritage(l:list[dict])->dict:
     return sorted(l, key=lambda x: x['patrimonio'])[-1]
 
+def add_id(data:list[dict]):
+    id = 0
+    for elem in data:
+        elem['id'] = id
+        id += 1
+    return data
+
+def sell(data:list[dict], value:float, client_id:int):
+    for client in data:
+        if client["id"] == client_id:
+            client["patrimonio"] = client["patrimonio"] - value
+
 
 def main():
     data = read_json('clienti.json')
+    data = add_id(data)
     new_client = {
         "nome": "Luca",
         "cognome": "Rossi",
         "eta": 100,
         "patrimonio": 840133.69,
-        "priorita": 6
+        "priorita": 6,
+        "id": 100
     }
+    sell(data, 1000000, 0)
     ordered = add_new_client(new_client, data)
 
     for row in ordered:
